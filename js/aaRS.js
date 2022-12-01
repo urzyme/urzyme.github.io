3d913a8f6f5a3b982d7e16546fea893bff952863
jordandouglas <jordandouglas@windowslive.com>
2022-12-01 21:16:38 +1300

    mobile css enhancements

--- js/aaRS.js
+++ js/aaRS.js
@@ -1,3 +1,6 @@
+IS_MOBILE = (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
+		   			 || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4)));
+
 
 
 DATA = {};
@@ -40,15 +43,18 @@ CHAIN_NAMES = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", 
                "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
 
-SVG_WIDTH = 800;
+
 NT_WIDTH = 10;
 NT_HEIGHT = 13;
 FEATURE_HEIGHT_ALN = 18;
 FEATURE_HEIGHT_SEC = 30;
+FEATURE_FONT_SIZE = 14;
 SEC_WIDTH = 1.0;
 SEC_HEIGHT = 20;
 NT_FONT_SIZE = 11;
 ALN_LABEL_WIDTH = 300;
+
+
 
 SELECT_FONT_FADEOUT_TIME = 10000;
 
@@ -64,10 +70,6 @@ CATALYTIC_DOMAIN_CUBIC_RIGHT_DX = 0;
 CATALYTIC_DOMAIN_LOOP_WIDTH = 3;
 CATALYTIC_DOMAIN_HELIX_CORNER_RADIUS = 7;
 
-LEVEL_1_COL = "#fa2a5599";
-LEVEL_2_COL = "#a6a6a6";
-LEVEL_3_COL = "#d3d3d3";
-LEVEL_4_COL = "#ffffff";
 
 STRAND_ARROW_HEAD_LEN_1 = 5;
 STRAND_ARROW_HEAD_LEN_2 = 6;
@@ -75,6 +77,35 @@ STRAND_ARROW_BASE_WIDTH = 7;
 STRAND_ARROW_HEAD_WIDTH = 13;
 HELIX_WIDTH = 11;
 HELIX_CORNER_RADIUS = 3;
+
+if (IS_MOBILE){
+	
+	let factor = 2;
+	NT_WIDTH *= factor+0.5;
+	NT_HEIGHT *= factor+0.5;
+	NT_FONT_SIZE *= factor+0.5;
+	SEC_WIDTH *= factor;
+	SEC_HEIGHT *= factor;
+	ALN_LABEL_WIDTH *= factor;
+	FEATURE_HEIGHT_ALN *= factor;
+	FEATURE_HEIGHT_SEC *= factor;
+	FEATURE_FONT_SIZE *= factor;
+	STRAND_ARROW_HEAD_LEN_1 *= factor;
+	STRAND_ARROW_HEAD_LEN_2 *= factor;
+	STRAND_ARROW_BASE_WIDTH *= factor;
+	STRAND_ARROW_HEAD_WIDTH *= factor;
+	HELIX_WIDTH *= factor;
+	HELIX_CORNER_RADIUS *= factor;
+	CATALYTIC_DOMAIN_WIDTH *= 1.25;
+	CATALYTIC_DOMAIN_HEIGHT *= 1.25;
+}
+
+LEVEL_1_COL = "#fa2a5599";
+LEVEL_2_COL = "#a6a6a6";
+LEVEL_3_COL = "#d3d3d3";
+LEVEL_4_COL = "#ffffff";
+
+
 
 function renderaaRS(isPairwise = false){
 
@@ -132,10 +163,15 @@ function renderaaRS(isPairwise = false){
 
 	// Tertiary dropdowns
 	$("#tertiaryTable").append("<span class='button' onClick='deselectSites(true)'>Clear selection</span>");
-  $("#tertiaryTable").append("<span class='dropdownDiv domainSelect'>Domain: <select id='domainSelect'></select></span>");
+	$("#tertiaryTable").append("<span class='dropdownDiv domainSelect'>Domain: <select id='domainSelect'></select></span>");
 	$("#tertiaryTable").append("<span class='dropdownDiv colouring'>Alignment colour: <select id='tertiaryColouringAln'></select></span>");
 	$("#tertiaryTable").append("<span class='dropdownDiv colouring'>Reference colour: <select id='tertiaryColouringSingle'></select></span>");
 	
+	
+	if (IS_MOBILE){
+		$("#tertiaryTable").find("span").after("<br>");
+		$("#tertiaryTable").find("span").css("display", "inline-block");
+	}
 	
 	// Domain selection
     let dropdown = $("#domainSelect");
@@ -710,9 +746,9 @@ function renderSecondary(svg){
   	  
       var text;
   	  if (feature == "Motif 3" || feature == "KMSKS"){
-  		   text = drawSVGobj(svgContent, "text", {lower: lower, upper:upper, x: x1+NT_WIDTH/4, y: y-SEC_HEIGHT/20, style: "cursor:pointer; text-anchor:end; dominant-baseline:hanging; font-size:14px; fill:" + textCol}, value=txt)
+  		   text = drawSVGobj(svgContent, "text", {lower: lower, upper:upper, x: x1+NT_WIDTH/4, y: y-SEC_HEIGHT/20, style: "cursor:pointer; text-anchor:end; dominant-baseline:hanging; font-size:" + FEATURE_FONT_SIZE + "px; fill:" + textCol}, value=txt)
   	  }else{
-  		   text = drawSVGobj(svgContent, "text", {lower: lower, upper:upper,  x: x1-NT_WIDTH/4, y: y-SEC_HEIGHT/20, style: "cursor:pointer; text-anchor:start; dominant-baseline:hanging; font-size:14px; fill:" + textCol}, value=txt)
+  		   text = drawSVGobj(svgContent, "text", {lower: lower, upper:upper,  x: x1-NT_WIDTH/4, y: y-SEC_HEIGHT/20, style: "cursor:pointer; text-anchor:start; dominant-baseline:hanging; font-size:" + FEATURE_FONT_SIZE + "px; fill:" + textCol}, value=txt)
   	  }
 
 
@@ -1208,20 +1244,16 @@ function renderAlignment(divID, isPrimary = true, downloadHref = ""){
 		  ctx.fillRect(x1-NT_WIDTH, y-FEATURE_HEIGHT_ALN/2, x2-x1, FEATURE_HEIGHT_ALN);
 		  ctx.stroke();
 		  ctx.strokeRect(x1-NT_WIDTH, y-FEATURE_HEIGHT_ALN/2, x2-x1, FEATURE_HEIGHT_ALN);
-		  
-        //drawSVGobj(svgAlign, "rect", {x: x1-NT_WIDTH, y: y-FEATURE_HEIGHT_ALN/2, width: x2-x1, height:FEATURE_HEIGHT_ALN, style:"stroke-width:0.7px; stroke:black; fill:" + col});
+
       }
 
 
 
   		// Text
   		ctx.fillStyle = textCol;
-  		ctx.font = "14px Source sans pro";
+  		ctx.font = FEATURE_FONT_SIZE + "px Source sans pro";
   		ctx.fillText(txt, x1-NT_WIDTH + (x2-x1)/2, y);
 
-
-
-		//drawSVGobj(svgAlign, "text", {x: x1-NT_WIDTH + (x2-x1)/2, y: y, style: "text-anchor:middle; dominant-baseline:central; font-size:16px; fill:" + textCol}, value=txt)
 
     }
 
